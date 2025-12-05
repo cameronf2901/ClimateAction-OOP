14ec5b550280c0569c2e840b745d901105a04c4d
Kamlesh0403 <kamlesh15067213@gmail.com>
2025-12-05 08:59:15 +0530

    Change package name from com.mycompany to climateactionapp

--- climateaction/ClimateInfo.java
+++ climateaction/ClimateInfo.java
@@ -1,8 +1,10 @@
+package climateactionapp;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.climateaction;
+
 
 /**
  *
@@ -69,3 +71,4 @@ public class ClimateInfo {
         return "Year: " + year + ", Sea Level: " + seaLevel + "mm, Temp Diff: " + temperatureDifference + "°C";
     }
 }
+
